97916ba6dae4559d1a5cb18c9adcb8197eb1c0c3
João Ferreira <jpbat@student.dei.uc.pt>
2015-04-06 22:51:52 +0100

    added 404 protecction

--- website/server.py
+++ website/server.py
@@ -1,31 +1,49 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import api
 import web
 import sys
 from web import form
 sys.path.append('services')
+import api
+import re
 
 render = web.template.render('templates/')
 
 urls = (
-	'/', 'index'
+	'/', 'index',
+	'/stat', 'stat'
 )
 
 userData = form.Form(
 	form.Textbox("name"), 
 	form.Dropdown('region', ['euw', 'na']),
-) 
+)
 
 class index:
 	def GET(self): 
 		form = userData()
 		return render.formtest(form)
 	def POST(self):
-		data = web.data() # you can get data use this method
-		return web.input()["name"], web.input()["region"]
+		return getGames(web.input()["region"], web.input()["name"])
+
+class stat:
+	def GET(self):
+		return "Hello World!"
+	def POST(self):
+		return getRandomStat(web.input()["region"], web.input()["summonerId"])
+
+def getGames(region, summonerName):
+	summonerId = api.getIdBySummonerName(region, summonerName)
+	return api.getUrfGames(region, summonerId)
+
+def getRandomStat(region, matchId):
+	return None
+
+def notfound():
+	return web.notfound("Sorry, Teemo mushrooms destroyed the page you are looking for :(")
 
 if __name__ == '__main__':
 	app = web.application(urls, globals())
+	app.notfound = notfound
 	app.run()
